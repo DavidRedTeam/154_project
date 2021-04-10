b91b6c007c6a1c328528cadcc9df72dba22188aa
Mitchell Agcaoili Maltezo <81717745+maltezoa@users.noreply.github.com>
2021-04-09 18:51:30 -0700

    Main Game of Life

---
+++ GoLmain.py
@@ -0,0 +1,21 @@
+#######################################################################
+#   Conway's Game of Life | CSCI 154
+#   
+#   Reference: https://electronut.in/simple-python-matplotlib-implementation-of-conways-game-of-life/
+#   
+#   Group: Mitchell Maltezo, Micah Mercado, David Andrade, Harpreet Ghag
+#
+#   Language: Python
+#
+#######################################################################
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+ beacon = [[1,1,0,0],
+           [1,1,0,0],
+           [0,0,1,1],
+           [0,0,1,1]]
