c741f582ba117089b375cacfd1f29e8b4706036a
Mitchell Agcaoili Maltezo <81717745+maltezoa@users.noreply.github.com>
2021-04-10 19:52:32 -0700

    Update GoLmain.py

--- GoLmain.py
+++ GoLmain.py
@@ -18,10 +18,10 @@ N = 10
 vals = [1, 0]
 
 # populate grid with random values with probability 3/10 for a live cell
-grid = np.random.choice(vals, N*N, p=[0.3,0.7]).reshape(N, N)
+#grid = np.random.choice(vals, N*N, p=[0.3,0.7]).reshape(N, N)
 
 # for all OFF values to test other objects, comment out line 24 and uncomment line 26
-#grid = np.random.choice(vals, N*N, p=[0,1]).reshape(N, N)
+grid = np.random.choice(vals, N*N, p=[0,1]).reshape(N, N)
 
 # Delete quotations for spaceship 
 """spaceship = [[0, 0, 1, 1, 0],
@@ -40,11 +40,11 @@ grid[1:5, 1:6] = spaceship"""
 grid[1:5, 1:5] = beacon"""
 
 # Delete quotations for glider
-"""glider = [[0, 0, 1, 0, 0],
+glider = [[0, 0, 1, 0, 0],
              [0, 0, 0, 1, 0],
              [0, 1, 1, 1, 0]]
 
-grid[1:4, 1:6] = glider"""
+grid[1:4, 1:6] = glider
 
 def update(data):
   global grid
