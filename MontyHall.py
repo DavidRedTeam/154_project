6a385eb40cd692d828776667a01a486b04c53619
HarpreetGh <70241088+HarpreetGh@users.noreply.github.com>
2021-04-15 00:47:58 -0700

    Varient setup, Graphs started

--- MontyHall.py
+++ MontyHall.py
@@ -11,10 +11,10 @@
 
 # import numpy as np
 from numpy import random
+import matplotlib.pyplot as plt
 
 # doorNum = int(input("Enter number of Doors to use in Monty Hall Simulation: "))
 # policyNum = int(input("Enter Policy Number: "))
-policyNum = 1
 
 
 class Game:
@@ -30,7 +30,7 @@ class Game:
             self.prize = random.randint(doorNum)
             winPer = 0.0
             for policy in self.policy:
-                print("Strategy: " + str(policy))
+                # print("Strategy: " + str(policy))
                 for count in self.count:
                     if policy == 0:
                         winPer = self.Policy0(count, doorNum)
@@ -46,7 +46,7 @@ class Game:
                         graphData1.append(
                             {
                                 "Strategy": policy,
-                                "Doors": doorNum,
+                                "Doors": str(doorNum),
                                 "Average Win Probability": winPer,
                             }
                         )
@@ -55,11 +55,11 @@ class Game:
                         graphData2.append(
                             {
                                 "Trials": count,
-                                "Doors": doorNum,
+                                "Doors": str(doorNum),
                                 "Average Win Propability": winPer,
                             }
                         )
-
+                    """
                     print(
                         "With "
                         + str(doorNum)
@@ -69,7 +69,7 @@ class Game:
                         + str(winPer)
                         + "%"
                     )
-                print("\n")
+                print("\n")"""
         return graphData1, graphData2
 
     def Policy0(self, count, doorNum):  # Experiment
@@ -133,7 +133,74 @@ class Game:
 
         return (right / count) * 100
 
+    def Policy3(self, count, doorNum):
+        # The flip of Sticking with original selection is...
+        # Randomly switch to one of the remaining doors
+        return 100 - self.Policy4(count, doorNum)
+
+        """right = 0
+        wrong = 0
+        choice = 0
+
+        for i in range(count):
+            choice = random.randint(doorNum)
+
+            slipDoor = choice
+            while choice == slipDoor:  # choice of player cannot equal random goat door
+                slipDoor = random.randint(doorNum)
+
+            if slipDoor == self.prize:
+                right += 1
+            elif choice == self.prize:
+                wrong += 1
+            else:
+                right += 1
+
+        return (right / count) * 100"""
+
+    def Policy4(self, count, doorNum):
+        # Host opens 1 door randomly
+        # 1-doorNum = doors possible with prize
+        # Stick with your original selection.
+
+        # 1/(1-doorNum) chance of winning
+        right = 0
+        wrong = 0
+        choice = 0
+
+        for i in range(count):
+            choice = random.randint(doorNum)
+            slipDoor = choice
+            while choice == slipDoor:  # choice of player cannot equal random goat door
+                slipDoor = random.randint(doorNum)
+            if choice == self.prize or slipDoor == self.prize:
+                right += 1
+            else:
+                wrong += 1
+
+        return (right / count) * 100
+
 
 Monty_Hall = Game()
 graphDatas = Monty_Hall.Start()
-print(graphDatas)
+# print(graphDatas)
+
+names1 = [x["Doors"] for x in graphDatas[0]]
+values1 = [x["Average Win Probability"] for x in graphDatas[0]]
+names2 = []
+# print(names, values)
+
+plt.figure(figsize=(9, 3))
+# plt.plot(names1, values1)
+plt.bar(names1, values1)
+plt.plot(names1, values1, "-o", color="orange")
+plt.ylabel("Average Win Percentage")
+plt.xlabel("Number of Doors")
+plt.suptitle("Strategy 1")
+plt.show()
+"""
+plt.plot([1, 2, 3, 4])
+
+
+plt.show()
+"""
