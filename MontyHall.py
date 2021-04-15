0ebe2fde8a97514e2edaf063078b7f6b72dbbb90
HarpreetGh <70241088+HarpreetGh@users.noreply.github.com>
2021-04-14 23:56:15 -0700

    Data Ready to Graph, Still need to do Varient

--- MontyHall.py
+++ MontyHall.py
@@ -14,34 +14,65 @@ from numpy import random
 
 # doorNum = int(input("Enter number of Doors to use in Monty Hall Simulation: "))
 # policyNum = int(input("Enter Policy Number: "))
-doorNum = 3
 policyNum = 1
 
 
 class Game:
-    def __init__(self, doorNum, policy):
-        self.policy = policy
-        self.doorNum = doorNum
-        self.prize = random.randint(doorNum)
-        # self.doors = ["goat" for x in range(doorNum)]
-        # self.doors[self.prize] = "prize"
-        #  #self.choice = int(input("Choose a Door 1-"+ str(self.doorNum) + ": "))
+    def __init__(self):
+        self.policy = [1, 2]
+        self.doorNum = [3, 6, 9, 20, 100]
+        self.count = [10, 50, 100]  # , 200, 500, 1000]
 
     def Start(self):
-        winPer = 0.0
-        count = 1000
-        if self.policy == 0:
-            winPer = self.Policy0(count)
-        elif self.policy == 1:
-            winPer = self.Policy1(count)
-        elif self.policy == 2:
-            winPer = self.Policy2(count)
-        else:
-            print("Invalid Policy choice Bye!")
-            return
-        print("How many times won: " + str(winPer) + "%")
+        graphData1 = []
+        graphData2 = []
+        for doorNum in self.doorNum:
+            self.prize = random.randint(doorNum)
+            winPer = 0.0
+            for policy in self.policy:
+                print("Strategy: " + str(policy))
+                for count in self.count:
+                    if policy == 0:
+                        winPer = self.Policy0(count, doorNum)
+                    elif policy == 1:
+                        winPer = self.Policy1(count, doorNum)
+                    elif policy == 2:
+                        winPer = self.Policy2(count, doorNum)
+                    else:
+                        print("Invalid Policy choice Bye!")
+                        return
 
-    def Policy0(self, count):  # Experiment
+                    if count == self.count[-1] and policy == 1:
+                        graphData1.append(
+                            {
+                                "Strategy": policy,
+                                "Doors": doorNum,
+                                "Average Win Probability": winPer,
+                            }
+                        )
+
+                    if doorNum == self.doorNum[-1] and policy == 2:
+                        graphData2.append(
+                            {
+                                "Trials": count,
+                                "Doors": doorNum,
+                                "Average Win Propability": winPer,
+                            }
+                        )
+
+                    print(
+                        "With "
+                        + str(doorNum)
+                        + " doors. With Count "
+                        + str(count)
+                        + " Win percentage: "
+                        + str(winPer)
+                        + "%"
+                    )
+                print("\n")
+        return graphData1, graphData2
+
+    def Policy0(self, count, doorNum):  # Experiment
         # Host opens 1 door randomly at a time
         # 1-doorNum = doors possible with prize
         # Randomly switch to one of the remaining doors
@@ -51,7 +82,7 @@ class Game:
         choice = 0
 
         for i in range(count):
-            doors = [x for x in range(self.doorNum)]
+            doors = [x for x in range(doorNum)]
             goats = doors.copy()
             goats.pop(self.prize)
             # goats is all doors that have goat
@@ -78,12 +109,12 @@ class Game:
 
         return (right / count) * 100
 
-    def Policy1(self, count):
+    def Policy1(self, count, doorNum):
         # The flip of Sticking with original selection is...
         # Randomly switch to one of the remaining doors
-        return 100 - self.Policy2(count)
+        return 100 - self.Policy2(count, doorNum)
 
-    def Policy2(self, count):
+    def Policy2(self, count, doorNum):
         # Host opens 1 door randomly
         # 1-doorNum = doors possible with prize
         # Stick with your original selection.
@@ -94,7 +125,7 @@ class Game:
         choice = 0
 
         for i in range(count):
-            choice = random.randint(self.doorNum)
+            choice = random.randint(doorNum)
             if choice == self.prize:
                 right += 1
             else:
@@ -103,6 +134,6 @@ class Game:
         return (right / count) * 100
 
 
-Monty_Hall = Game(doorNum, policyNum)
-Monty_Hall.Start()
-print()
+Monty_Hall = Game()
+graphDatas = Monty_Hall.Start()
+print(graphDatas)
